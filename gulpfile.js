e1220ffd00ba2fc6559010c8c1cca9918119ed6f
Raaj108 <rajj108@outlook.com>
2019-08-26 16:16:45 -0700

    added gulp and sass dependencies to the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-const defaultTask = (cb) => {
-  cb();
-}
+'use strict';
 
-exports.default = defaultTask
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+
+sass.compiler = require("node-sass");
+
+gulp.task('sass', () => {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', () => {
+    gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
+});
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store/Store';
